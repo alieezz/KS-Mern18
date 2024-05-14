cc21ad0df2b161c4f6cf0bd0d9232be8dfccf81e
alieezzz <ali.haider18@hotmail.com>
2024-05-13 18:17:47 -0700

    Committing the work

---
+++ prac.js
@@ -0,0 +1,20 @@
+// let num1 = prompt("Enter a number : ")
+// const square = (num1)=>{
+// return (num1*num1);
+// }
+
+// square(5)
+// console.log(square)
+const A = () =>{
+    setTimeout(c=>{
+        console.log("1")
+    },4000)
+} 
+
+const B = () =>{
+    setTimeout(c=>{
+        console.log("2")
+    },4000)
+}
+A ()
+B ()
